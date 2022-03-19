5714a57e2a242c7985fdc9dd15812aa20ceb2ed2
akmaljon <akmaljon.ahmedjanov@yandex.ru>
2022-03-19 21:01:43 +0500

    add single

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+//single.html
 const hambur = document.querySelector('.header__mobile-menu');
 const menu = document.querySelector('.header__menu');
 const closeBtn = document.querySelector('.header__closed--button');
@@ -11,6 +11,22 @@ hambur.addEventListener('click', (e) => {
 
 closeBtn.addEventListener('click', () => {
     menu.classList.remove('header__menu--on')
+    // curtain.classList.remove('curtain--on')
+})
+
+//index.html
+const hammenu = document.querySelector('.header__mobile-menu');
+const menyu = document.querySelector('.header__main-menu');
+const close = document.querySelector('.header__closed--button');
+// const curtain = document.querySelector('.curtain');
+
+hammenu.addEventListener('click', (e) => {
+    menyu.classList.add('header__main-menu--on');
+    // curtain.classList.add('curtain--on')
+})
+
+close.addEventListener('click', () => {
+    menyu.classList.remove('header__main-menu--on')
     // curtain.classList.remove('curtain--on')
 })
 
